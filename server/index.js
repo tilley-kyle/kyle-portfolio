f2e0a51f7bff81e1c5e9c3f436d3afaf0446ede8
Kyle Tilley <katilley10@gmail.com>
2020-07-06 17:20:06 -0600

    need to change the port for the server so whatever port it is online it will still work

--- server/index.js
+++ server/index.js
@@ -5,4 +5,4 @@ const port = 8153;
 
 app.use(express.static('build'));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`));
