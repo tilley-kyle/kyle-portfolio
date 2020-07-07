f22578ef41904905c2dad42a337e7ea21448cd77
Kyle Tilley <katilley10@gmail.com>
2020-07-07 15:21:45 -0600

    removed TopBanner component, it wasn't necessary

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 import './App.css';
 
-import TopBanner from './components/TopBanner';
+import Home from './components/Home';
 
 class App extends React.Component {
   constructor (props) {
@@ -21,7 +21,20 @@ class App extends React.Component {
   render() {
     return (
       <div className="total-container">
-        <TopBanner />
+        <Router>
+        <div className="banner-container">
+          <Link to="/">Home</Link>
+          <Link to="/About">About Kyle</Link>
+          <Link to="/Code">Kyle's Code</Link>
+          <Link to="/Photos">Photos a la Kyle</Link>
+        </div>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
         <div className="temp">Coming Soon</div>
       </div>
     );
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+  <div className="temp">home</div>
+)
+
+export default Home;
