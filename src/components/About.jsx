bda0ac2c6fcdb86fd095552ddca7594b0630a299
Kyle Tilley <katilley10@gmail.com>
2020-07-08 12:59:33 -0600

    added a photo of me and Juno for a temp placeholder

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './About.css';
+import img from '../images/me_and_juno.jpg';
 
 const About = () => (
   <div className="about-container">
     <div className="left-column"></div>
     <div className="center-column">
-      
+      <img className="about-image" src={img} alt="Juno and me" />
+      <h1 className="about-header">Who Is Kyle?</h1>
     </div>
     <div className="right-column"></div>
   </div>
