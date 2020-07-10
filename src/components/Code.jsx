956b83aff539aba1e84ce3bb699dbce55e4ed011
Kyle Tilley <katilley10@gmail.com>
2020-07-10 13:26:16 -0600

    added technologies into state manually. Will make a route to populate a database later

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -6,20 +6,26 @@ class Code extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      JavaScript: [],
-      backEnd: [],
-      dataBase: [],
-      generic: [],
+      JavaScript: ['JavaScript(ES5/ES6)', 'React', 'React-Router', 'Webpack', 'Babel', 'Axios', 'HTML', 'CSS', 'JQuery'],
+      backEnd: ['Node.js', 'Express.js', 'Redis', 'Socket.IO'],
+      dataBase: ['MongoDB', 'MySQL'],
+      testing: ['Mocha', 'Chai'],
+      generic: ['Git', 'npm', 'AWS', 'Heroku', 'Bash'],
     }
   }
 
+  componentDidMount() {
+    //get request to database that will store my techs to make for easier input of new techs learned
+  }
+
   render() {
+    const { JavaScript, backEnd, dataBase, testing, generic } = this.state;
     return (
       <div className="code-container">
         <div className="left-column"></div>
         <div className="center-column">
           <h1>Welcome to the Code of Kyle</h1>
-          <CodeTechStack />
+          <CodeTechStack JavaScript={JavaScript} backEnd={backEnd} dataBase={dataBase} testing={testing} generic={generic} />
         </div>
         <div className="right-column"></div>
       </div>
