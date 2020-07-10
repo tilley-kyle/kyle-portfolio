839e732fdd8010d3bfc0dc840f389ba0acd8309f
Kyle Tilley <katilley10@gmail.com>
2020-07-10 11:44:41 -0600

    general layout for code page put in place

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './Code.css';
 
 const Code = () => (
-  <div className="temp">Code coming soon</div>
+  <div className="code-container">
+    <div className="left-column"></div>
+    <div className="center-column">
+      <div className="temp">Code coming soon</div>
+    </div>
+    <div className="right-column"></div>
+  </div>
 );
 
 export default Code;
