751b4ac6a3b6e2b2f54a66b015a935566964eb8f
Kyle Tilley <katilley10@gmail.com>
2020-07-10 14:10:21 -0600

    got bullet points infront of my divs for each tech. I didn't want to use list elements as built in for formatting reasons

--- src/components/CodeTechStack.jsx
+++ src/components/CodeTechStack.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import './CodeTechStack.css';
 
-const CodeTechStack = () => {
+const CodeTechStack = ({ JavaScript, backEnd, dataBase, testing, generic }) => {
 
   return (
-    <table>
-      
-    </table>
+    <div className="tech-container">
+      <h3 className="tech-title">JavaScript</h3>
+        {JavaScript.map((tech) => (
+          <div className="tech-item">{tech}</div>
+        ))}
+    </div>
   )
 };
 
