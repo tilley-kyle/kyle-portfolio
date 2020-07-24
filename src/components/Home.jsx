1f0825e1ef399b22bfe3e0651008fb99c8c98579
Kyle Tilley <katilley10@gmail.com>
2020-07-24 16:31:07 -0600

    Chagned the text in the home page sections and changed font-sizes to fixed sizes. Will be adding in breakpoints later

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,35 +7,37 @@ const Home = () => (
     <div className="left-column"></div>
     <div className="center-column">
     <img className="fricken" src={image} alt="where is the Fricken?"/>
-      <h1 className="title1">Welcome to the world of Kyle</h1>
+      <h1 className="title1">Welcome to the Website O' Kyle</h1>
       <div className="top-paragraph">
-        Here we might find information as to why this site might exist. Will this site be for the Code of Kyle? Will it be for view the world as Kyle
-        attempts to capture it through the lenses of a DSLR? Maybe we will learn who Kyle is in a section that describes what this site is About. What about a blog-like section, talking
-        about the comings and goings of Kyle's life in Code, photography, or other interests? One thing we do know is that we don't actually know what is to come. This is just a filler paragraph you read.
+        Thank you for visitng my site! I am building this to practice my skills as a software engineer and showcase things that I have done
+        in engineering, as well as in other parts of my life from running to photography! I'm using Agile methedologies to put up this site so there
+        is always a product to see! Follow along, see who I am, and checkout how my site grows!
       </div>
       <div className="recent-container">
         <h3>What is next for Kyle</h3>
-        Here we might find some information on what Kyle has planned next in the pipeline. Is he going to build a cool new app? Learn a crazy new tech? Run another ultra-marathon?
-        Maybe he'll take a sweet shot of the Milky Way? Who knows, but yet again you are reading filler.
+        I have a bunch of things I am itching to get to from engineering to brewing! In no particular order...
         <ul className="ul">
-          <li className="li">'About Kyle' component: add photos and nav bar</li>
-          <li className="li">Build out 'My Code' component</li>
+          <li className="li">Finish database app for Campro USA</li>
+          <li className="li">Fill out 'About Kyle' and 'My Code' components</li>
+          <li className="li">Work on sizing, incorporate breakpoints</li>
           <li className="li">Build out 'Photography' component</li>
-          <li className="li">Put finalized info into each component</li>
-          <li className="li">blog like component?</li>
-          <li className="li">Get back to training</li>
-          <li className="li">Put a recently completed list on the home page, celebrate my baller-status!</li>
+          <li className="li">blog like component? Less likely</li>
+          <li className="li">Master Redux in React</li>
+          <li className="li">Determine real running goals and create a plan</li>
+          <li className="li">Practice light painting on the Mesa trail</li>
+          <li className="li">Decide next beer to brew</li>
         </ul>
       </div>
       <div className="recent-container">
-        <h3>What has Kyle done recently</h3>
-        In this section we will list the things Kyle has recently completed. Did he just go on an adventure?
-        Did he just complete classes? Learn any new technologies? Finish a race or a hard workout?
+        <h3>Recent Reasons to Revel</h3>
+        Simple as the header, but if I must explain it, I'd say "This is a list of highlighted things I have accomplished recently."
         <ul className="ul">
-          <li className="li">finished Galvanize</li>
-          <li className="li">SDC</li>
-          <li className="li">MVP</li>
-          <li className="li">Firts time-lapse</li>
+          <li className="li">GRADUATED FROM GALVANIZE/HACK REACTOR!!</li>
+          <li className="li">Built an API that can handle thousands of requests per second</li>
+          <li className="li">Made Battleship and learned how to use websockets!</li>
+          <li className="li">Firts time-lapse, and did it during sunset into night!</li>
+          <li className="li">Got back into endurance sports with running and cycling!</li>
+          <li className="li">Started a freelance job creating a database app!</li>
         </ul>
       </div>
     </div>
