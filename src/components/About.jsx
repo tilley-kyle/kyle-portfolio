1c19646b0315dcd7e86af892ad3bbcc7b410b567
Kyle Tilley <katilley10@gmail.com>
2020-07-24 16:48:32 -0600

    changed some of the contenet on the 'about kyle' page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => (
       <h1 className="about-header">Who Is Kyle?</h1>
       <div className="about-top-paragraph">
         You've found your way onto my site, either on purpose or by stumbling here following random links as you explore deeper and deeper into the World Wide Web.
-        Either way you are here now, so let me tell you who I am! I'm a energetic person who thinks 'type 2' fun is the best time! I program, run, do photography, brew better
+        Either way you are here now, so let me tell you who I am! I'm a energetic person who thinks 'type 2' fun is the best time! I program, run, photograph, brew beer
         and try to use my time to the fullest!
       </div>
       <div className="about-interest-container">
@@ -19,13 +19,13 @@ const About = () => (
         at Galvanize/Hack Reactor. There I learned:
         <ul className="about-ul">
           <li className="li">Coding best practices</li>
-          <li className="li">JS and its popular technologies</li>
-          <li className="li">Soft skills like pair programing and Agile workflow</li>
+          <li className="li">JS and its popular frameworks and technologies</li>
+          <li className="li">Soft skills such as pair programing and Agile workflow</li>
           <li className="li">How to autonomously and elegently solve challenges</li>
           <li className="li">Grit through working 70+ hours per week constantly facing new technologies, topics, and problems</li>
         </ul>
-        I'm always looking for what I can learn next. I'm never satisfied with going through the motions and staying stagnant with what I know. I am happiest
-        when I am contributing real code that is making real change for the better!
+        I'm always looking for what I can learn next. I'm never satisfied with going through the motions and staying stagnant with what I know. Who has 2 thumbs and is happiest
+        when contributing real code that is making real change for the better? THIS GUY!
       </div>
       <div className="about-interest-container">
         <h3>Runner</h3>
@@ -35,14 +35,14 @@ const About = () => (
       </div>
       <div className="about-interest-container">
         <h3>Dog Dad</h3>
-        Juno, who you can see on the top of this page along side a bearded version of myself and gracing the top banner of every page here, is my best friend since adopting her
+        Juno, who you can see on the top of this page along side a bearded version of myself and gracing the top banner of every page here, has been my best friend since adopting her
         Feb 7th, 2018. Rescued from Chataqua Park in Boulder as a puppy, she has been at my side running, doing agility, and most importantly playing fetch.
       </div>
       <div className="about-interest-container">
         <h3>Photographer</h3>
         Do I think I'm creative? Yes. Do I like to enjoy the beauty of Nature? Yes. Do I like activities that take a lot of trial/error and practice? Yes.
         Do I like using technology to solve problems? Yes. <br/> Well those are the reasons why I love photographing landscapes, the night sky, wild animals
-        and Juno alike!
+        and Juno!
       </div>
       <div className="about-interest-container">
         <h3>Brewer</h3>
