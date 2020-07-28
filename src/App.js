950980f75bc0ebd0a234e6fc73baefcdd9c45df2
Kyle Tilley <katilley10@gmail.com>
2020-07-28 14:35:41 -0600

    reformmatted how the link container is set up so I can add links to the right side of the screen

--- src/App.js
+++ src/App.js
@@ -19,22 +19,28 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      seekingJob: true,
     }
   }
 
 
   render() {
+    const { seekingJob } = this.state;
     return (
       <div className="total-container">
         <Router>
           <div className="banner-container">
-          <img className="logo" src={logo} alt="logo here"/>
-            <div className="link-container">
-              <Link className="link" to="/">Home</Link>
-              <Link className="link" to="/About">About Kyle</Link>
-              <Link className="link" to="/Code">My Code</Link>
-              <Link className="link" to="/Photo">Photography</Link>
+            <img className="logo" src={logo} alt="logo here" />
+            <div className="header-bar">
+              <div className="link-container">
+                <Link className="link" to="/">Home</Link>
+                <Link className="link" to="/About">About Kyle</Link>
+                <Link className="link" to="/Code">My Code</Link>
+                <Link className="link" to="/Photo">Photography</Link>
+              </div>
+              <div className="about-me">
+                
+              </div>
             </div>
           </div>
           <Switch>
