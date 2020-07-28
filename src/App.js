1d423bcf99ff138813f6fb9f3230a15a137e6897
Kyle Tilley <katilley10@gmail.com>
2020-07-28 15:02:44 -0600

    finally got the right side links formatted correctly

--- src/App.js
+++ src/App.js
@@ -26,20 +26,22 @@ class App extends React.Component {
 
   render() {
     const { seekingJob } = this.state;
+    const hiringButtonClassName = seekingJob ? 'green' : 'gray';
     return (
       <div className="total-container">
         <Router>
           <div className="banner-container">
             <img className="logo" src={logo} alt="logo here" />
             <div className="header-bar">
+            <div className="about-me">
+                <button className={hiringButtonClassName}>Is Kyle Job Seeking?</button>
+                <Link className="link" to="/contactInfo">info</Link>
+              </div>
               <div className="link-container">
                 <Link className="link" to="/">Home</Link>
                 <Link className="link" to="/About">About Kyle</Link>
                 <Link className="link" to="/Code">My Code</Link>
                 <Link className="link" to="/Photo">Photography</Link>
-              </div>
-              <div className="about-me">
-                
               </div>
             </div>
           </div>
