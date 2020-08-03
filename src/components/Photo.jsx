6f3ec2d8f7429f854663c6d5887f87cef4def98e
Kyle Tilley <katilley10@gmail.com>
2020-08-03 12:25:32 -0600

    changed the Photo component from functional to class

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import './Photo.css';
 import image from '../images/Under-Construction.png';
 
-const Photo = () => (
-  <div>
-  <img className="temp-img" src={image} alt="under construction" />
-  <div className="temp">Photography coming soon</div>
-  </div>
-);
+class Photo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <img className="temp-img" src={image} alt="under construction" />
+        <div className="temp">Photography coming soon</div>
+      </div>
+    )
+  }
+};
 
 export default Photo;
