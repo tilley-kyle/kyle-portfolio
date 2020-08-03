84d5551eaa398ef6628f90ffc86c90a6da9a5c85
Kyle Tilley <katilley10@gmail.com>
2020-08-03 16:35:22 -0600

    found out how to embed a photo from flickr

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import PhotoDisplay from './PhotoDisplay';
 import './Photo.css';
-import image from '../images/Under-Construction.png';
 
 class Photo extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       flickrPhotos: [],
+      selectPhotos: [],
     }
   }
 
@@ -17,7 +16,20 @@ class Photo extends React.Component {
       return data.json();
     })
     .then((photos) => {
-      this.setState({ flickrPhotos: photos.photos.photo });
+      const randomNumbers = [];
+      for (let i = 0; i < 6; i ++) {
+        let newNumber = Math.floor(Math.random() * 30);
+        while (randomNumbers.indexOf(newNumber) > -1) {
+          newNumber = Math.floor(Math.random() * 29);
+        }
+        randomNumbers.push(newNumber);
+      }
+      const selectPhotos = [];
+      randomNumbers.forEach((number) => selectPhotos.push(photos.photos.photo[number]));
+      this.setState({
+        flickrPhotos: photos.photos.photo,
+        selectPhotos: selectPhotos
+      });
     })
     .catch((err) => {
       console.log('boo', err);
@@ -30,7 +42,10 @@ class Photo extends React.Component {
       <div className="photo-container">
         <div className="left-column"></div>
         <div className="center-column">
-          <PhotoDisplay />
+          <div className="photo-heading">
+            <img className="heading-photo" src="https://farm66.staticflickr.com/65535/50061978293_5b11e4f89a.jpg" alt="test" />
+            <p>;lkjsdflkjsd</p>
+          </div>
         </div>
         <div className="right-column"></div>
       </div>
@@ -39,3 +54,7 @@ class Photo extends React.Component {
 };
 
 export default Photo;
+{/* <a data-flickr-embed="true" href="https://www.flickr.com/photos/kyleatilley/50061978293/in/dateposted-public/" title="Prarie Sunflower and Cleveland Peak">
+  <img src="https://live.staticflickr.com/65535/50061978293_309b0f5fc3_k.jpg" width="2048" height="1132" alt="Prarie Sunflower and Cleveland Peak" />
+</a>
+<script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script> */}
